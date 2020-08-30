504b6b3d9ecab0f19b68654110742ae6c6ac09f0
Adan Scotney <adan.scotney@gmail.com>
2020-08-30 10:23:05 +0100

    Timer emulation

--- source/timer.c
+++ source/timer.c
@@ -4,17 +4,32 @@
 #include <sys/time.h>
 #include "dolos2x.h"
 
-static struct timeval startTime;
+static struct timeval lastTickTime;
 
 static uint32_t* rTCOUNT = NULL;
 
 static void handleTimerGet(uc_engine *uc, uc_mem_type type, uint64_t address, int size, int64_t value, void *user_data) {
-  printf("Handling tcount get\n");
+  time_t lastSec = lastTickTime.tv_sec;
+  suseconds_t lastUsec = lastTickTime.tv_usec;
+  gettimeofday(&lastTickTime, NULL);
+
+  unsigned long long elapsedNsecs = ((lastTickTime.tv_sec - lastSec) * 1000000000) + ((lastTickTime.tv_usec - lastUsec) * 1000);
+  unsigned long long elapsedTicks = (elapsedNsecs / 135);
+  *rTCOUNT += elapsedTicks;
+  #ifdef DEBUG
+  printf("Access to TCOUNT, elapsed ticks since last access %u, count is 0x%x\n", elapsedTicks, *rTCOUNT);
+  #endif
+}
+
+static void handleTimerSet(uc_engine *uc, uc_mem_type type, uint64_t address, int size, int64_t value, void *user_data) {
+  gettimeofday(&lastTickTime, NULL);
+  *rTCOUNT = value;
 }
 
 int initTimer() {
-  gettimeofday(&startTime, NULL);
-  rTCOUNT = (uint32_t*) ((uint8_t*)getIORegs())+TCOUNT;
+  gettimeofday(&lastTickTime, NULL);
+  rTCOUNT = ((uint32_t*)getIORegs())+(TCOUNT>>2);
   
   hookRegRead(REG(TCOUNT), 4, handleTimerGet);
+  hookRegWrite(REG(TCOUNT), 4, handleTimerSet);
 }
